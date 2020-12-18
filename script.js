7c1b2b9e2b40043feb02775d628af3785edad52b
ter127 <74223273+ter127@users.noreply.github.com>
2020-12-18 09:40:16 +0900

    .

--- script.js
+++ script.js
@@ -1,56 +1,37 @@
 window.onload = function(){
     var contain = document.querySelector('.contain');
     var trans = [
-        {'classname':'.newsbtn1',
+        {'classname':'newsbtn1',
         'translate':'0'},
-        {'classname':'.newsbtn2',
+        {'classname':'newsbtn2',
         'translate':'-100'},
-        {'classname':'.newsbtn3',
+        {'classname':'newsbtn3',
         'translate':'-200'},
-        {'classname':'.newsbtn4',
+        {'classname':'newsbtn4',
         'translate':'-300'},
-        {'classname':'.newsbtn5',
+        {'classname':'newsbtn5',
         'translate':'-400'}
     ]
-    document.querySelector('.newsbtn1').addEventListener('click',function(){
-        contain.style.transform = 'translate(0vw)'
-        document.querySelector('.newsbtn1').style.backgroundColor = 'rgb(80, 80, 80)'
-        document.querySelector('.newsbtn2').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn3').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn4').style.backgroundColor = 'rgb(216, 213, 213)' 
-        document.querySelector('.newsbtn5').style.backgroundColor = 'rgb(216, 213, 213)'
-    })
-    document.querySelector('.newsbtn2').addEventListener('click',function(){
-        contain.style.transform = 'translate(-100vw)'
-        document.querySelector('.newsbtn1').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn2').style.backgroundColor = 'rgb(80, 80, 80)'
-        document.querySelector('.newsbtn3').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn4').style.backgroundColor = 'rgb(216, 213, 213)' 
-        document.querySelector('.newsbtn5').style.backgroundColor = 'rgb(216, 213, 213)'
-    })
-    document.querySelector('.newsbtn3').addEventListener('click',function(){
-        contain.style.transform = 'translate(-200vw)'
-        document.querySelector('.newsbtn1').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn2').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn3').style.backgroundColor = 'rgb(80, 80, 80)'
-        document.querySelector('.newsbtn4').style.backgroundColor = 'rgb(216, 213, 213)' 
-        document.querySelector('.newsbtn5').style.backgroundColor = 'rgb(216, 213, 213)'
-    })
-    document.querySelector('.newsbtn4').addEventListener('click',function(){
-        contain.style.transform = 'translate(-300vw)'
-        document.querySelector('.newsbtn1').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn2').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn3').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn4').style.backgroundColor = 'rgb(80, 80, 80)' 
-        document.querySelector('.newsbtn5').style.backgroundColor = 'rgb(216, 213, 213)'
-    })
-    document.querySelector('.newsbtn5').addEventListener('click',function(){
-        contain.style.transform = 'translate(-400vw)'
-        document.querySelector('.newsbtn1').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn2').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn3').style.backgroundColor = 'rgb(216, 213, 213)'
-        document.querySelector('.newsbtn4').style.backgroundColor = 'rgb(216, 213, 213)' 
-        document.querySelector('.newsbtn5').style.backgroundColor = 'rgb(80, 80, 80)'
-    })
-        
+    var a = 0
+    function slid() {
+        if (a<0){
+            a = a -100
+            contain.style.transform = 'translate('+a+'vw)'
+                    }
+        if (a==-400) {
+            contain.style.transform = 'translate(100vw)'
+        }           
+}
+setTimeout(slid,5000)
+
+    for (var index in trans) {
+        var paragraph = document.createElement("div");
+        document.getElementById("btnbox").appendChild(paragraph);
+        paragraph.classList.add('newsbtn')
+        paragraph.classList.add(trans[index]['classname'])
+        paragraph.addEventListener('click',function(){
+            
+        contain.style.transform = 'translate('+trans[index]['translate']+'vw)'
+        })
+    }     
 }
