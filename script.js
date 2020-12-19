0ac0e8308022ea375021bf341246d44c19a640f2
yangpa043 <yangpa043@gmail.com>
2020-12-19 16:25:21 +0900

    ^^

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ window.onload = function () {
     mb1.addEventListener('mouseover', function () {
         mb1.style.color = "rgb(137, 137, 209)";
         mb1.style.fontSize = "17px";
-    mb1.addEventListener('mouseout', function () {
-        mb1.style.color = "white";
-        mb1.style.fontSize = "15px";
+        mb1.addEventListener('mouseout', function () {
+            mb1.style.color = "white";
+            mb1.style.fontSize = "15px";
+        });
+        mb2.addEventListener('mouseover', function () {
+            mb2.style.color = "rgb(137, 137, 209)";
+            mb2.style.fontSize = "17px";
+        });
+        mb2.addEventListener('mouseout', function () {
+            mb2.style.color = "white";
+            mb2.style.fontSize = "15px";
+        });
+        mb3.addEventListener('mouseover', function () {
+            mb3.style.color = "rgb(137, 137, 209)";
+            mb3.style.fontSize = "17px";
+        });
+        mb3.addEventListener('mouseout', function () {
+            mb3.style.color = "white";
+            mb3.style.fontSize = "15px";
+        });
+        mb4.addEventListener('mouseover', function () {
+            mb4.style.color = "rgb(137, 137, 209)";
+            mb4.style.fontSize = "17px";
+        });
+        mb4.addEventListener('mouseout', function () {
+            mb4.style.color = "white";
+            mb4.style.fontSize = "15px";
+        });
+        setInterval(function () {
+            if (-5200 < a) {
+                a = a - 1300
+                contain.style.transform = 'translate(' + a + 'px)'
+            }
+        }, 5000)
     });
-    mb2.addEventListener('mouseover', function () {
-        mb2.style.color = "rgb(137, 137, 209)";
-        mb2.style.fontSize = "17px";
-    });
-    mb2.addEventListener('mouseout', function () {
-        mb2.style.color = "white";
-        mb2.style.fontSize = "15px";
-    });
-    mb3.addEventListener('mouseover', function () {
-        mb3.style.color = "rgb(137, 137, 209)";
-        mb3.style.fontSize = "17px";
-    });
-    mb3.addEventListener('mouseout', function () {
-        mb3.style.color = "white";
-        mb3.style.fontSize = "15px";
-    });
-    mb4.addEventListener('mouseover', function () {
-        mb4.style.color = "rgb(137, 137, 209)";
-        mb4.style.fontSize = "17px";
-    });
-    mb4.addEventListener('mouseout', function () {
-        mb4.style.color = "white";
-        mb4.style.fontSize = "15px";
-    });
-    setInterval(function(){
-        if (-5200<a) {
-            a = a - 1300
-            contain.style.transform = 'translate('+a+'px)'
-        }
-        },5000)
-});
 
 
     var contain = document.querySelector('.contain');
-    document.querySelector('.newsbtn1').addEventListener('click',function(){
+    document.querySelector('.newsbtn1').addEventListener('click', function () {
         contain.style.transform = 'translate(0)'
     })
-    document.querySelector('.newsbtn2').addEventListener('click',function(){
+    document.querySelector('.newsbtn2').addEventListener('click', function () {
         contain.style.transform = 'translate(-1300px)'
     })
-    document.querySelector('.newsbtn3').addEventListener('click',function(){
+    document.querySelector('.newsbtn3').addEventListener('click', function () {
         contain.style.transform = 'translate(-2600px)'
     })
-    document.querySelector('.newsbtn4').addEventListener('click',function(){
+    document.querySelector('.newsbtn4').addEventListener('click', function () {
         contain.style.transform = 'translate(-3900px)'
     })
-    document.querySelector('.newsbtn5').addEventListener('click',function(){
+    document.querySelector('.newsbtn5').addEventListener('click', function () {
         contain.style.transform = 'translate(-5200px)'
     })
     document.getElementById("logo").addEventListener('click', function () {
         window.scrollTo(0, 0);
     })
     document.getElementById("mb1").addEventListener('click', function () {
-        window.scrollTo(0, 1100);
+        window.scrollTo(0, 870);
     })
     document.getElementById("mb2").addEventListener('click', function () {
-        window.scrollTo(0, 2500);
+        window.scrollTo(0, 2660);
     })
     document.getElementById("mb3").addEventListener('click', function () {
         window.scrollTo(0, 0);
@@ -74,18 +74,18 @@ window.onload = function () {
     document.getElementById("mb4").addEventListener('click', function () {
         window.scrollTo(0, 0);
     })
-    
-    setInterval(function(){
+
+    setInterval(function () {
         var a = 0
-        if (-5200<a) {
+        if (-5200 < a) {
             a = a - 1300
-            contain.style.transform = 'translate('+a+'px)'
+            contain.style.transform = 'translate(' + a + 'px)'
         }
-        },5000)    
-        $("document").ready(function() {  
-            $(window).scroll(function() {  
-                    var position = $(window).scrollTop(); // 현재 스크롤바의 위치값을 반환합니다.  
-            });   
-        }); 
+    }, 5000)
+    $("document").ready(function () {
+        $(window).scroll(function () {
+            var position = $(window).scrollTop(); // 현재 스크롤바의 위치값을 반환합니다.  
+        });
+    });
 
 }
